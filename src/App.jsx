0c2ff0de33b3103fa43caafa91dd492bebac3149
NishthaSharma-22 <nishthapinjore@gmail.com>
2025-06-11 22:28:48 +0530

    all of styling and favicon update

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,27 @@ const App = () => {
     <div className="app">
       <section className="side-bar">
         <button className="button">+ New Chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+          <li>Chat 1</li>
+          <li>Chat 2</li>
+        </ul>
         <nav>
-          <p>Made by Nishtha</p>
+          <p>GryffinTalk</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>GryffinTalk</h1>
+        <ul className="talk-feed"></ul>
+        <div className="bottom-section">
+          <div className="input-box">
+            <input />
+            <div id="submit">Submit</div>
+          </div>
+          <p className="info">
+            GryffinTalk v1.0.0 <br />Bridging the world of muggles and magic.
+          </p>
+        </div>
+      </section>
     </div>
   );
 
