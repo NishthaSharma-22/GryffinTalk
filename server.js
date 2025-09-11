13d3ba01276223b59dab786e8c77ce557d5e7541
Nishtha Sharma <nishthapinjore@gmail.com>
2025-09-11 12:42:58 +0530

    switched to hc api

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 const PORT = 2200;
-const apiKey = process.env.API_KEY;
+// const apiKey = process.env.API_KEY;
 
 app.use(cors());
 app.use(express.json());
@@ -16,11 +16,11 @@ app.post("/completions", async (req, res) => {
   const options = {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${apiKey}`,
+      // Authorization: `Bearer ${apiKey}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      model: "gpt-3.5-turbo",
+      // model: "gpt-3.5-turbo",
       messages: [
         {
           role: "system",
@@ -34,16 +34,17 @@ app.post("/completions", async (req, res) => {
   };
   try {
     const response = await fetch(
-      "https://api.openai.com/v1/chat/completions",
+      "https://ai.hackclub.com/chat/completions",
       options
     );
     const data = await response.json();
     res.send(data);
   } catch (error) {
     console.error(error);
+    res.status(500).send({error: "something went wrong with the ai request! :("})
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Your server is running on PORT ${PORT}`);
+  console.log(`wizarding server is running on PORT ${PORT}`);
 });
